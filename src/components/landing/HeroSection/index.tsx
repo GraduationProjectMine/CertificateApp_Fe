0647e61d2d09b84f9ff10cc8cabb03d4b99a4568
DarkinTran <darkintran2005@gmail.com>
2026-07-16 23:22:35 +0700

    test: update verified-by text

--- src/components/landing/HeroSection/index.tsx
+++ src/components/landing/HeroSection/index.tsx
@@ -132,7 +132,7 @@ export default function HeroSection() {
                 </div>
 
                 <p className={styles._39} style={{ marginTop: "0.5rem", opacity: 0.6 }}>
-                  Verified by CertiChain
+                  Verified by NguyenTT
                 </p>
               </div>
             </div>
